7eb5d9d35a16b7a0bdddcf8021b755590b0371f0
Tomas Agustin Silva Nuñez <tomassilvanunez@gmail.com>
2025-05-29 18:13:51 -0300

    Agregar archivos de proyecto.

---
+++ GuitarraStore.Data/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GuitarraStore.Modelos;
+
+namespace GuitarraStore.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Guitarras> Guitarras { get; set; }
+    }
+}
---
+++ GuitarraStore.Modelos/Guitarras.cs
@@ -0,0 +1,10 @@
+﻿namespace GuitarraStore.Modelos
+{
+    public class Guitarras
+    {
+        public string ?Marca { get; set; }
+        public string ?Modelo { get; set; }  
+        public float Precio { get; set; }
+
+    }
+}
